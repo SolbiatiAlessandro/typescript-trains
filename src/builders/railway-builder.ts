367a4baad3de61e1709537aff7897ddcfb1a9ca6
Alessandro Solbiati <lessandro@fb.com>
2022-02-15 00:57:55 +0000

    [railway] group factor, still not cleaning old

--- src/builders/railway-builder.ts
+++ src/builders/railway-builder.ts
@@ -3,14 +3,12 @@ import {Point} from '../objects/point';
 import {Node} from '../objects/node';
 import {ControlPoint} from './control-point';
 import {MainScene} from '../scenes/main-scene';
-import {Rail} from '../builders/rail';
+import {IRailway} from '../interfaces/IRailway.interface';
 
-export class RailwayBuilder extends Phaser.GameObjects.Group {
-	edges: Array<Edge> = []; 
+export class RailwayBuilder {
+	private railways: Array<IRailway> = [];
 
-	constructor(public scene: MainScene){
-		super(scene);
-	}
+	constructor(public scene: MainScene){};
 
 	newRailway(){
 	  const edge = new Edge({
@@ -19,17 +17,20 @@ export class RailwayBuilder extends Phaser.GameObjects.Group {
 		  secondControlPoint: new ControlPoint(this.scene),
 		  endNode: new Node()
 	  });
-	  this.edges.push(edge);
+	  this.railways.push({
+		  edge: edge,
+		  buildingGroup: new Phaser.GameObjects.Group(this.scene)
+	  })
 	}
 
-	paintRailway(edge: Edge, _debug: boolean = false){
-	  edge._railwayPoints(50).forEach(
+	paintRailway(railway: IRailway, _debug: boolean = false){
+	  railway.edge._railwayPoints(50).forEach(
 		  ([point, tangent]) => {
 			  if(_debug){
 				  let _p = new Point(point.x, point.y); _p._debug(this.scene, "blue");
 				  let _t = new Point(point.x + (20 * tangent.x), point.y + (20 * tangent.y)); _t._debug(this.scene, "orange");
 			  }
-			  const rail = this.getFirstDead(true, point.x, point.y, 'rail');
+			  const rail = railway.buildingGroup.getFirstDead(true, point.x, point.y, 'rail');
 			  rail.rotation = Phaser.Math.Angle.Between(0, 0, tangent.x, tangent.y) + (Phaser.Math.PI2 / 4);
 			  rail.displayHeight = 32;
 			  rail.displayWidth = 64;
@@ -38,8 +39,6 @@ export class RailwayBuilder extends Phaser.GameObjects.Group {
 	}
 
 	update(){
-		for(const edge of this.edges){
-			this.paintRailway(edge);
-		}
+		this.railways.forEach(railway => this.paintRailway(railway));
 	}
 }
---
+++ src/interfaces/IRailway.interface.ts
@@ -0,0 +1,6 @@
+import {Edge} from '../objects/edge'
+
+export interface IRailway {
+	edge: Edge,
+	buildingGroup: Phaser.GameObjects.Group
+}
