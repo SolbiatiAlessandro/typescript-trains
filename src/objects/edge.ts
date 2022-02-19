5491d0c61f1fe2537993fbf670cf95eabc1f2130
Alessandro Solbiati <lessandro@fb.com>
2022-02-19 04:37:15 +0000

    [railway][1/n] constraints on railway controller not moving too much
    looks bad if user can move the railway too much around, break the
    simulation.
    To implement this we first need to have edges and then something like
    
    onDrag(x, y)
    compute if this x,y is possible (no distance breaking, no curvature
    breaking)
    if it's not don't call the update railway method
    
    how to check if x,y is possible? should this be a second test curve?
    I like this design. Two curves, one with railway, one without.
    Maybe the second curve is a property of the edge? Edge doesn't extend
    anymore curve but has

--- src/objects/edge.ts
+++ src/objects/edge.ts
@@ -2,7 +2,10 @@ import {Node} from './node'
 
 export class Edge extends Phaser.Curves.CubicBezier{
 	segments: number;
+	breakingEmitter: Phaser.Events.EventEmitter;
 	private readonly LENGTH_SEGMENT_RATIO: number = 15;
+	private readonly BREAKING_DISTANCE: number = 5;
+	private readonly BREAKING_DISTANCE_EVENT: string = 'breaking_distance';
 
 	constructor(startNode: Node, endNode: Node){
 		super(
@@ -12,12 +15,21 @@ export class Edge extends Phaser.Curves.CubicBezier{
 			endNode.vector
 		);
 		this.segments = Math.floor(this.getLength() / this.LENGTH_SEGMENT_RATIO);
+		this.breakingEmitter = new Phaser.Events.EventEmitter();
+	}
+
+	_emitBreakingDistance(firstPoint: Phaser.Math.Vector2, secondPoint: Phaser.Math.Vector2): Phaser.Math.Vector2 {
+		if(Phaser.Math.Distance.BetweenPoints(firstPoint, secondPoint) > this.BREAKING_DISTANCE){
+			this.breakingEmitter.emit(this.BREAKING_DISTANCE_EVENT);
+		}
+		return secondPoint;
 	}
 
 
 	points(): 
 		Array<[Phaser.Math.Vector2, Phaser.Math.Vector2]>{
 		const points: Array<Phaser.Math.Vector2> = this.getPoints(this.segments);
+		points.reduce((previousPoint, currentPoint) => this._emitBreakingDistance(previousPoint, currentPoint), points[0]);
 		return Array(this.segments + 1).fill(0).map(
 			(_, i) => {
 				var _t = this.getTangent(i / (this.segments + 1));
