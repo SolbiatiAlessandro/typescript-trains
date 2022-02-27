3bb043e41023550eb6493b33225f0eab569ead4c
Alessandro Solbiati <lessandro@fb.com>
2022-02-27 23:49:54 +0000

    [select railway] deselect

--- src/events/graph-selection.ts
+++ src/events/graph-selection.ts
@@ -5,17 +5,20 @@ import { Node } from '../objects/node';
 export class GraphSelection extends Phaser.Events.EventEmitter{
 	private readonly SELECT_EDGE_EVENT: string = "_SELECT_EDGE";
 	//graphBuilder not public, can't be accessed by railway
-	constructor(graphBuilder: GraphBuilder){
+	constructor(public graphBuilder: GraphBuilder){
 		super();
-		this.on(this.SELECT_EDGE_EVENT, function(railway: Railway){
+		this.on(this.SELECT_EDGE_EVENT, function(railway: Railway, show: boolean){
 			// make node appear
 			this.scene.player.selected = railway;
 			let nodes: Array<Node> = this.getNodesFromEdge(railway.key);
-			nodes.forEach(node => node.display());
+			nodes.forEach(node => node.display(show));
 		}, graphBuilder)
 	}
 
 	selectEdge(railway: Railway){
-		this.emit(this.SELECT_EDGE_EVENT, railway);
+		if (typeof this.graphBuilder.scene.player.selected != 'undefined'){
+			this.emit(this.SELECT_EDGE_EVENT, this.graphBuilder.scene.player.selected, false);
+		}
+		this.emit(this.SELECT_EDGE_EVENT, railway, true);
 	}
 }
